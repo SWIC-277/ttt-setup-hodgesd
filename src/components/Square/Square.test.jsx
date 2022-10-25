90d3ad24648f80fd14f3eaec3871cbf0a9ec42c0
Derrick Hodges <hodgesd@gmail.com>
2022-10-25 17:46:40 -0500

    feat: :sparkles: add button

---
+++ src/components/Square/Square.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Square from './Square';
+
+it("renders a square with the correct marker ('x' or 'o')", () => {
+  // Arrange
+  render (<Square marker="X" />);
+
+  // Act
+  const square = screen.getByRole('button' {name: "X"});
+
+  // Assert
+  expect(square).toBeInTheDocument();
+
+});
+
